2d40460be0aa57872d7c8f7ae4f57309c314b5ff
Franco Caraballo <franconcaraballo@gmail.com>
2024-10-23 20:26:00 -0300

    Fix url base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://francocaraballo.git.io/portfolio',
+  base: 'https://francocaraballo.github.io/portfolio',
 })
